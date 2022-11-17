d0e1a8c5bd692085979ecbc9de4e73e04e69d611
Firdavs <firdavs_20111@mail.ru>
2022-11-17 22:58:03 +0300

    merge develop into master

--- app/src/main/java/com/firdavs/ecommerce/MainActivity.kt
+++ app/src/main/java/com/firdavs/ecommerce/MainActivity.kt
@@ -6,38 +6,33 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.firdavs.ecommerce.ui.theme.EcommerceTheme
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.firdavs.api.LocalDataProvider
+import com.firdavs.common.di.LocalCommonProvider
+import com.firdavs.common.ui.theme.EcommerceTheme
+import com.firdavs.ecommerce.di.LocalAppProvider
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
             EcommerceTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    CompositionLocalProvider(
+                        LocalAppProvider provides application.appProvider,
+                        LocalDataProvider provides application.appProvider,
+                        LocalCommonProvider provides application.appProvider
+                    ) {
+                        Navigation()
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    EcommerceTheme {
-        Greeting("Android")
     }
 }
